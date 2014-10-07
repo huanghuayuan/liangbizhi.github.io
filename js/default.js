db120002b27f972b6df2027fc64f8b0b0628e8b1
liangbizhi <liangbizhi1993@gmail.com>
2014-10-07 08:34:08 +0800

    change pages style

--- js/default.js
+++ js/default.js
@@ -3,18 +3,28 @@
  */
 $(document).ready(function(){
 	var nav		= $(".nav");
+	var logo	= $(".logo");
 	var backToTop	= $(".back-to-top");
 	backToTop.hide();
 	backToTop.click(function(){
 		$("html, body").animate({scrollTop:0}, "fast");
+	});
+	logo.click(function(){
+		window.location.href = "/";
 	});
 	//height of 'header' & following 'nav'.
 	var topHeight	= nav.height();
 	$(window).scroll(function(){
 		if($(window).scrollTop() > topHeight){
 			nav.addClass("nav-scroll");
+			logo.removeClass("logo");
+			logo.addClass("logo-scroll");
+			logo.text("必苦其心志");
 		}else{
 			nav.removeClass("nav-scroll");
+			logo.removeClass("logo-scroll");
+			logo.addClass("logo");
+			logo.text("");
 		}
 		if($(window).scrollTop() > (topHeight + 100)){
 			backToTop.fadeIn("fast");
