cd5ae477ee2ac0221e992e39623d1b282c548908
liangbizhi <liangbizhi1993@gmail.com>
2014-10-19 00:18:35 +0800

    about me was done. the first article

---
+++ js/about.js
@@ -0,0 +1,54 @@
+var secondes = 0;
+var preSecond = 0;
+
+$(document).ready(function(){
+	init();
+	var $myFace = $("#myface");
+	$myFace.click(function(){
+		playAudio();
+	});
+});
+
+/**
+ * Initialize the UI.
+ */
+function init(){
+	var $lyric = $("#lyric");
+	var $descTxt = $(".desc");
+	$descTxt.hide();
+	$lyric.hide();
+	$descTxt.fadeIn("slow");
+}
+/**
+ * Play the music.
+ */
+function playAudio(){
+	var audio = document.getElementById("myaudio");
+	if(audio){
+		$("#myface").fadeOut("slow");
+		audio.play();
+		$(".desc").text("《爱してるばんぎーい》");
+		$("#lyric").fadeIn("slow");
+		setInterval(highlightLyric, 1000);
+	}else{
+		$(".desc").text("HTML5 is not supported");
+	}
+}
+
+/**
+ * highlight the current lyric.
+ */
+function highlightLyric(){
+	secondes++;
+	var currentLineLyric = document.getElementById(secondes + "s");
+	if(currentLineLyric){
+		console.log(secondes);
+		/* select the old highlight lyric line and remove the class attribute to remove the highlight style*/
+		var $preLineLyric = $("#" + preSecond + "s");
+		if($preLineLyric){
+			$preLineLyric.removeAttr("class");
+		}
+		$(currentLineLyric).attr("class", "lyric-hl");
+		preSecond = secondes;
+	}
+}
